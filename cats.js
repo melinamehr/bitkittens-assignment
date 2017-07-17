34a745c1a564a1f083dcc5de4988a25508ec6f42
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-07-17 13:47:36 -0400

    kittens added into div but same kitten in every div

--- cats.js
+++ cats.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  var addCat = document.querySelector('.summon-cats')
+  var addCat = document.querySelector('.summon-cats');
 
   addCat.addEventListener('click', function() {
     console.log('adding cat button works')
@@ -9,19 +9,13 @@ document.addEventListener("DOMContentLoaded", function() {
       method: 'GET',
       dataType: 'json'
     }).done(function(responseData) {
-      // Creates a new <img> tag
-      var image = document.createElement('img')
-      var divBox = document.querySelectorAll('.cat-box')
-      // Sets the src attribute of the <img> to the cat's photo
-      image.setAttribute('src', responseData.cats[0].photo)
-      // Inserts that <img> into one of the empty .cat-box divs
-      divBox[0].appendChild(image)
-      image.alt = 'Photo of' + responseData.cats[0].name
-
-
-
-// Sets the alt attribute of the <img> to "Photo of (insert cat name here)"
-
-    })
-  })
+        for(i = 0; i < responseData.cats.length; i++) {
+        var image = document.createElement('img');
+        var divBox = document.querySelectorAll('.cat-box');
+        image.setAttribute('src', responseData.cats[0].photo)
+        image.alt = 'Photo of ' + responseData.cats[0].name;
+        divBox[i].appendChild(image);
+      };
+    });
+  });
 });
